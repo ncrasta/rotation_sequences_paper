a2799b1303c89b42f9614182fec90e76827e7d05
Naveen Crasta <naveen.crasta@gmail.com>
2023-02-16 21:59:17 +0100

    Update Dh.m

--- ICC/code/viviani_2/Dh.m
+++ ICC/code/viviani_2/Dh.m
@@ -2,9 +2,10 @@
 % ANIMATION OF VIVIANIS CURVE USING THE NORMAL PARAMETRIZATION
 % Authos: Dr. S. P. Bhat and Naveen
 % MATLAB verison: 2014a
-% External files needed: mArrow3
-% Last modified: December 24, 2014 
 % For the explanation refer to the paper "Closed Attitude Trajectories"
 %%=======================================================================%%
+
+% Derivative of h(t,a)
+
 function Dx3 = Dh(t,a)
 Dx3 = -3*pi*t^2/a^3 +3*pi*t/a^2;
