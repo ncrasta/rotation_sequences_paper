f6b015c7cc405e5fcccac2087ee75b216013fb42
Naveen Crasta <naveen.crasta@gmail.com>
2023-02-16 22:00:06 +0100

    Update Dg1.m

--- ICC/code/viviani_2/Dg1.m
+++ ICC/code/viviani_2/Dg1.m
@@ -2,9 +2,10 @@
 % ANIMATION OF VIVIANIS CURVE USING THE NORMAL PARAMETRIZATION
 % Authos: Dr. S. P. Bhat and Naveen
 % MATLAB verison: 2014a
-% External files needed: mArrow3
-% Last modified: December 24, 2014 
 % For the explanation refer to the paper "Closed Attitude Trajectories"
 %%=======================================================================%%
+
+% Derivative of g1(t)
+
 function Dx1 = Dg1(t)
 Dx1 = -(3/(2*pi^2))*t^2 + (3/pi)*t;
