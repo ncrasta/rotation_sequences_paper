c595982149081b951a75269c128cdec21bcf7194
Naveen Crasta <naveen.crasta@gmail.com>
2023-02-16 21:49:57 +0100

    Create su.m

---
+++ ICC/code/viviani_1/su.m
@@ -0,0 +1,11 @@
+%%=======================================================================%%
+% ANIMATION OF VIVIANIS CURVE USING THE NORMAL PARAMETRIZATION
+% Authos: Dr. S. P. Bhat and Naveen
+% MATLAB verison: 2014a
+% External files needed: mArrow3
+% Last modified: December 24, 2014 
+% For the explanation refer to the paper "Closed Attitude Trajectories"
+% NOTE: PLEASE MAXIMIZE THE FIGURE WHEN IT APPEARS
+%%=======================================================================%%
+function s = su(t,a)
+s = a*sqrt(1+cos(t/2)^2);
