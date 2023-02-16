b57788acd17df6db1116424a24f32b4023fb6a5e
Naveen Crasta <naveen.crasta@gmail.com>
2023-02-16 21:48:20 +0100

    Create ku.m

---
+++ ICC/code/viviani_1/ku.m
@@ -0,0 +1,10 @@
+%%=======================================================================%%
+% ANIMATION OF VIVIANIS CURVE USING THE NORMAL PARAMETRIZATION
+% Authos: Dr. S. P. Bhat and Naveen
+% MATLAB verison: 2014a
+% External files needed: mArrow3
+% Last modified: December 24, 2014 
+% For the explanation refer to the paper "Closed Attitude Trajectories"
+%%=======================================================================%%
+function k = ku(t,a)
+k = -a*sin(0.5*t)*(2+cos(0.5*t)^2)/(1+cos(0.5*t)^2);
