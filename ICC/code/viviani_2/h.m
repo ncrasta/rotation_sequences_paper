e2a19aa66d4e4a15533fdf696481a2c94b660807
Naveen Crasta <naveen.crasta@gmail.com>
2023-02-16 21:54:17 +0100

    Create h.m

---
+++ ICC/code/viviani_2/h.m
@@ -0,0 +1,10 @@
+%%=======================================================================%%
+% ANIMATION OF VIVIANIS CURVE USING THE NORMAL PARAMETRIZATION
+% Authos: Dr. S. P. Bhat and Naveen
+% MATLAB verison: 2014a
+% External files needed: mArrow3
+% Last modified: December 24, 2014 
+% For the explanation refer to the paper "Closed Attitude Trajectories"
+%%=======================================================================%%
+function x3 = h(t,a)
+x3 = -pi*(t/a)^3 + (3*pi/2)*(t/a)^2;
